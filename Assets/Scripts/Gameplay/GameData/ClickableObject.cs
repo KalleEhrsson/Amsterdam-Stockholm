480cd3b7eb2d16b7cbd46a74d70c27425fb4242f
Yk_ <bo.bolweg@gmail.com>
2025-11-13 10:33:14 +0100

    item update

--- Assets/Scripts/Gameplay/GameData/ClickableObject.cs
+++ Assets/Scripts/Gameplay/GameData/ClickableObject.cs
@@ -1,14 +1,19 @@
 using NUnit.Framework;
 using UnityEngine;
 using System.Collections.Generic;
+using NUnit.Framework.Constraints;
 
 public class ClickableObject : MonoBehaviour
 {
+    [SerializeField] private GameManager gameManager;
     private MeshRenderer meshR;
 
     [Header("Materials")]
     [SerializeField] private List<Material> outlineMaterials;
     [SerializeField] private List<Material> normalMaterials;
+
+    [SerializeField] private int itemID;
+    [SerializeField] private int itemLevel;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -34,6 +39,7 @@ public class ClickableObject : MonoBehaviour
 
     private void OnMouseDown()
     {
+        gameManager.CollectItem(itemLevel, itemID);
         Destroy(this.gameObject);
     }
 }
